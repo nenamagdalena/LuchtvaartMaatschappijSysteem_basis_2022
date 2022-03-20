e35a519ab9dc5c5d74f74e275e37a87db1d027bd
Thenoxius <thomasvanrens@gmail.com>
2022-03-20 14:34:52 +0100

    localdate toegevoegd

--- src/test/VluchtTest.java
+++ src/test/VluchtTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import java.time.LocalDate;
 import java.util.Calendar;
 
 public class VluchtTest {
